6101a0f14f726ab0a3ad2033beed807d4e2d6763
othmane adari <othmane.adari@sap.com>
2018-07-28 20:21:05 -0400

    add Category Repository

---
+++ ShoppingCartBackend/src/main/java/com/othsoft/dao/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.othsoft.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.othsoft.model.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Long>{
+
+}
