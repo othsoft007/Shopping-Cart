a6e81ef781706a976252540680a49a1fb188187f
othmane adari <othmane.adari@sap.com>
2018-08-04 19:02:17 -0400

    Add routing

--- ShoppingCartUI/src/app/app.module.ts
+++ ShoppingCartUI/src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +18,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
